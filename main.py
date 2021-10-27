7db5c791ea092a0a1893cac10a677b19616fc868
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-10-27 23:12:14 +0000

    1st attempt. It's working

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+modulo = number % 2
+if modulo > 0:
+  print("This number is odd")
+else:
+  print("This number is even")
 
 
 
