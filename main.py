1cac187a686edef0277ab8079f7d484562b2ac50
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-10-27 23:19:15 +0000

    Changed to only use 1 variable and the ==

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-modulo = number % 2
-if modulo > 0:
-  print("This number is odd")
-else:
+if number % 2 == 0:
   print("This number is even")
+else:
+  print("This number is odd")
 
 
 
